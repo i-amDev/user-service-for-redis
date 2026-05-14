1d84c138e2c08cd03ece941f99e82629c3a838e3
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-05-14 15:23:35 +0530

    - Initial commit. - Created a user service project to learn about integrating "Redis".

---
+++ src/main/java/com/learning/user_service_for_redis/UserServiceForRedisApplication.java
@@ -0,0 +1,13 @@
+package com.learning.user_service_for_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceForRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceForRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/user_service_for_redis/UserServiceForRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.user_service_for_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceForRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
