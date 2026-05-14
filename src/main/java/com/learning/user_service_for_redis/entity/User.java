f90d18e57ef306ff883c0e1c3866307dc98ec4e7
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-05-14 16:40:46 +0530

    - Created "User" entity class.

---
+++ src/main/java/com/learning/user_service_for_redis/entity/User.java
@@ -0,0 +1,36 @@
+package com.learning.user_service_for_redis.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "user_entity")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String city;
+    private String country;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
